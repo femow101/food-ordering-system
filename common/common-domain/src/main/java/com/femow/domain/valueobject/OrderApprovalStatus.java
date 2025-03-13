f5aab104d6cbe36a874081a04a07592eabcaef25
femow101 <felipemu92@gmail.com>
2025-03-13 13:23:40 -0300

    Add DTOs to order application service

---
+++ common/common-domain/src/main/java/com/femow/domain/valueobject/OrderApprovalStatus.java
@@ -0,0 +1,5 @@
+package com.femow.domain.valueobject;
+
+public enum OrderApprovalStatus {
+    APPROVED, REJECTED
+}
---
+++ common/common-domain/src/main/java/com/femow/domain/valueobject/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.femow.domain.valueobject;
+
+public enum PaymentStatus {
+    COMPLETED,
+    CANCELLED,
+    FAILED
+}
