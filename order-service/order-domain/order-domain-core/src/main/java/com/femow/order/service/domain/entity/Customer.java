ab3ecdc430c975d27806e1786133c523d19d5cd3
femow101 <felipemu92@gmail.com>
2025-03-12 23:00:59 -0300

    Add customer and restaurant entities

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/femow/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.femow.order.service.domain.entity;
+
+import com.femow.domain.entity.AggregateRoot;
+import com.femow.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
