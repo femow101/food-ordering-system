102307853d11450250e4b0f4e05d581e1b1e8224
femow101 <felipemu92@gmail.com>
2025-03-13 12:54:53 -0300

    Create order events and create orderDomainService interface

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/femow/order/service/domain/event/OrderCancelledEvent.java
@@ -0,0 +1,12 @@
+package com.femow.order.service.domain.event;
+
+import com.femow.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCancelledEvent extends OrderEvent  {
+
+    public OrderCancelledEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
