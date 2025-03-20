e950fd4822b72f3df458ad5a7576c82a90766163
femow101 <felipemu92@gmail.com>
2025-03-19 23:33:24 -0300

    Adding Controller Advice on Application module

---
+++ common/common-application/src/main/java/com/femow/application/handler/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.femow.application.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
+    public ErrorDTO handleException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ErrorDTO.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .message("Unexpected error!")
+                .build();
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = ValidationException.class)
+    public ErrorDTO handleException(ValidationException validationException) {
+        String errorMessage = (validationException instanceof ConstraintViolationException)
+            ? extractViolationsFromException((ConstraintViolationException) validationException)
+            : validationException.getMessage();
+        log.error(errorMessage, validationException);
+
+        return ErrorDTO.builder()
+                .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(errorMessage)
+                .build();
+    }
+
+    private String extractViolationsFromException(ConstraintViolationException validationException) {
+        return validationException.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("--"));
+    }
+}
